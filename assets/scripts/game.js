571998e79f0bc602501ca118594c52964c63a7ad
LarryHillyer <LarryHillyer@hotmail.com>
2016-01-13 14:26:26 -0700

    Created Update function

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,7 +1,12 @@
 var health = 100;
+var healthElem = document.getElementById("health");
 
 function slap() {
     health--;   
-    var num = health.toString();
-    alert("Your fitness level is: "+ num);
+    /*var num = health.toString();
+    alert("Your fitness level is: "+ num);*/
 }
+function update() {
+    healthElem.innerHTML = health.toString();
+} 
+update();  
