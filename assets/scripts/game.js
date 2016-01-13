258eb1e921b4bcc0ccb75881b27dab6681a35785
LarryHillyer <LarryHillyer@hotmail.com>
2016-01-13 15:12:53 -0700

    Finished Step 7

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,17 +1,33 @@
 var health = 100;
-var healthElem = document.getElementById("health");
 
+var healthElem = document.getElementById("health");
+var hits = 0;
+var name = "RockinSockinMike";
+var nameElem = document.getElementById("name");
+var hitsElem = document.getElementById("hits");
 function slap() {
     health--; 
+    hits++;
     update();  
     /*var num = health.toString();
     alert("Your fitness level is: "+ num);*/
 }
 function update() {
     healthElem.innerText = health.toString();
+    nameElem.innerText = name;
+    hitsElem.innerText = hits.toString();
+    
 } 
 
 function punch() {
+    hits++;
+    health = health - 5;
+    update();
+}
+
+function kick() {
+    hits++;
+    health = health - 10;
     update();
 }
 update();  
