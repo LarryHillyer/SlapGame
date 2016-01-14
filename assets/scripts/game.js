d792780013f433ed89985812705ec53ca28ad7a7
LarryHillyer <LarryHillyer@hotmail.com>
2016-01-13 20:09:13 -0700

    Added gameOver  function

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,26 +1,19 @@
-var health = 100;
-
-var healthElem = document.getElementById("health");
-var hits = 0;
 var name = "RockinSockinMike";
+var health = 100;
+var hits = 0;
+
 var nameElem = document.getElementById("name");
+var healthElem = document.getElementById("health");
 var hitsElem = document.getElementById("hits");
-var healthProgBarElem = document.getElementById("health-1")
+
+var healthProgBarElem = document.getElementById("health-1");
+var hitsProgBarElem = document.getElementById("hits-1");
+
 function slap() {
     health--; 
     hits++;
     update();  
-    /*var num = health.toString();
-    alert("Your fitness level is: "+ num);*/
 }
-function update() {
-    healthElem.innerText = health.toString();
-    nameElem.innerText = name;
-    hitsElem.innerText = hits.toString();
-    $('.progress-bar').css('width', health +'%').attr('aria-valuenow', health);
-    healthProgBarElem.innerText = health.toString() + "%";
-    
-} 
 
 function punch() {
     hits++;
@@ -33,5 +26,43 @@ function kick() {
     health = health - 10;
     update();
 }
-$('.progress-bar').css('width', health .toString()+'%').attr('aria-valuenow', health.toString());
+
+function checkGameOver() {
+    if (health <= 0) {
+        alert("Game Over! ");
+        hits = 0;
+        health = 100;
+        updateHelper();
+    } 
+}
+
+function updateName() {
+    nameElem.innerText = 'Name: ' + name;
+}
+
+function updateHealth () {
+    healthElem.innerText = "Health: " + health.toString() + "%";
+    $('.progress-bar-health').css('width', health +'%').attr('aria-valuenow', health);
+    healthProgBarElem.innerText = health.toString() + "%";     
+}
+
+function updateHits() {
+    hitsElem.innerText = "Hits: " + hits.toString();
+    $('.progress-bar-hits').css('width', hits +'%').attr('aria-valuenow', hits);
+    hitsProgBarElem.innerText = hits.toString();  
+}
+
+function updateHelper() {
+    updateName();
+    updateHealth();
+    updateHits();
+}
+
+function update() {
+    updateHelper();    
+    checkGameOver();    
+}
+ 
+$('.progress-bar-health').css('width', health .toString()+'%').attr('aria-valuenow', health.toString());
+$('.progress-bar-hits').css('width', hits +'%').attr('aria-valuenow', hits);
 update();  
