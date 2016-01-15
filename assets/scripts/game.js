d17eb49fd7296a84cac0ebe3adf574bb48cc7add
LarryHillyer <LarryHillyer@hotmail.com>
2016-01-14 17:02:56 -0700

    Added Defenses

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -13,32 +13,32 @@ var hitsProgBarElem = document.getElementById("hits-1");
 var firstTime = true;
 
 function slap() {
-    health--; 
-    hits++;
+    player.health = player.health - 1*(1-player.addMods()); 
+    player.hits++;
     update();  
 }
 
 function punch() {
-    hits++;
-    health = health - 5;
+    player.hits++;
+    player.health = player.health - 5*(1 - player.addMods());
     update();
 }
 
 function kick() {
-    hits++;
-    health = health - 10;
+    player.hits++;
+    player.health = player.health - 10*(1-player.addMods());
     update();
 }
 
 function checkGameOver() {
-    if (health <= 0) {
+    if (player.health <= 0) {
         alert("Game Over! ");
-        hits = 0;
-        health = 100;
+        player.hits = 0;
+        player.health = 100;
         updateHelper();
         document.getElementById("player-panel").classList.remove("panel-danger")
         
-    } else if (health <= 10 && health > 0) {
+    } else if (player.health <= 10 && player.health > 0) {
         document.getElementById("player-panel").classList.add("panel-danger")
     } else {
         document.getElementById("player-panel").classList.remove("panel-danger")
@@ -46,19 +46,19 @@ function checkGameOver() {
 }
 
 function updateName() {
-    nameElem.innerText = 'Name: ' + name;
+    nameElem.innerText = 'Name: ' + player.name;
 }
 
 function updateHealth () {
-    healthElem.innerText = "Health: " + health.toString() + "%";
-    $('.progress-bar-health').css('width', health +'%').attr('aria-valuenow', health);
-    healthProgBarElem.innerText = health.toString() + "%";     
+    healthElem.innerText = "Health: " + player.health.toString() + "%";
+    $('.progress-bar-health').css('width', player.health +'%').attr('aria-valuenow', player.health);
+    healthProgBarElem.innerText = player.health.toString() + "%";     
 }
 
 function updateHits() {
-    hitsElem.innerText = "Hits: " + hits.toString();
-    $('.progress-bar-hits').css('width', hits +'%').attr('aria-valuenow', hits);
-    hitsProgBarElem.innerText = hits.toString();  
+    hitsElem.innerText = "Hits: " + player.hits.toString();
+    $('.progress-bar-hits').css('width', player.hits +'%').attr('aria-valuenow', player.hits);
+    hitsProgBarElem.innerText = player.hits.toString();  
 }
 
 function updateHelper() {
@@ -84,7 +84,38 @@ function update() {
     firstTime = false;    
     checkGameOver();    
 }
+
+function Item(name, modifier, description) {
+    this.name = name;
+    this.modifer = modifier;
+    this.description = description;
+    this.draw = function() {
+        
+    }   
+}
+
+function Player(name, health, hits, items) {
+    this.name=name;
+    this.health = health;
+    this.hits = hits;
+    this.items = items;
+    
+    this.addMods = function () {
+        var addMod1 = 0;
+        for (var i in this.items) {
+            addMod1 = addMod1 + this.items[i].modifer;      
+        }
+        return addMod1;
+    }
+    
+    
+}
+var items = {shield:new Item("Shield",0.3,"This is an awesome shield!"),
+            bodyArmor: new Item("Body Armor", 0.4, "This is what I use to rob banks"),
+            helmet: new Item("Helmet", 0.25, "This is the best Helmet available")}
+            
+var player = new Player(name, health, hits, [items.helmet, items.bodyArmor]);
  
-$('.progress-bar-health').css('width', health .toString()+'%').attr('aria-valuenow', health.toString());
-$('.progress-bar-hits').css('width', hits +'%').attr('aria-valuenow', hits);
-update();  
+$('.progress-bar-health').css('width', player.health.toString()+'%').attr('aria-valuenow', player.health.toString());
+$('.progress-bar-hits').css('width', player.hits +'%').attr('aria-valuenow', player.hits);
+update(); 
